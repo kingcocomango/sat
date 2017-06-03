449dd76b2831a96f2f577b9e76245e0f06a7362e
Mango <mango@localhost.localdomain>
2017-06-03 19:13:29 +0200

    Cleaned up holder

--- src/Holder.jsx
+++ src/Holder.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import PropTypes from "prop-types";
 import {Card,CardTitle,CardText} from "react-md/lib/Cards";
 
-var Holder = () => (<Card className="IntroCard"> <CardTitle title="Stuff" /> <CardText>Placeholder</CardText> </Card>);
+var Holder = (props) => (<Card className="IntroCard"> <CardTitle title="Stuff" /> <CardText>Placeholder. Contract deployed at {props.inst.address} </CardText> </Card>);
+
+Holder.propTypes = {
+	inst: PropTypes.object,
+};
 
 export default Holder;
